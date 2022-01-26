5c42612896a4d956cbb1cedff143c9580aefd3a4
Justuks <justinas.valutis@gmail.com>
2022-01-26 20:16:10 +0000

    kol kas pareguliavau nuli ir incrementus

--- naujas.js
+++ naujas.js
@@ -1,8 +1,12 @@
 let wind = 0
 let display = document.getElementById('wind-display')
-console.log(display)
 function up(){
-    display.innerHTML = wind
-    console.log(wind++)
+ wind = ++wind
+ display.innerHTML = wind
 }
 up()
+function down(){
+    wind = --wind
+    display.innerHTML = wind
+}
+down()
