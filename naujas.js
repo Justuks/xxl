73b8ea610b64ec7bf381f589e6fc7895e10ef188
Justuks <justinas.valutis@gmail.com>
2022-01-29 16:02:46 +0000

    sutvarkiau rpm increase

--- naujas.js
+++ naujas.js
@@ -5,12 +5,27 @@ let display = document.getElementById('wind-display')
 function up(){
     if(wind > 29)return false
    console.log(display.innerHTML = ++wind)
-
-
 }
 up()
+
 function down(){
     if(wind < 1)return false
     console.log(display.innerHTML = --wind)
 }
 down()
+
+let rpm = 600
+let rpmDisplay = document.getElementById('rpm-display')
+
+function high(){
+    if(rpm > 2300)return false
+    console.log(rpmDisplay.innerHTML = rpm += 100)
+}
+high()
+
+
+function low(){
+    if(rpm < 600)return false
+     console.log(displayRpm.innerHTML = rpm - 100)
+}
+lower()
